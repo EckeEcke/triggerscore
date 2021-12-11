4c9f92759a1252a9bd3a1662e94f01df9662045b
Christian Eckardt <eckardt.christian@web.de>
2021-12-11 15:01:24 +0100

    scrolltop on routechange

--- src/main.js
+++ src/main.js
@@ -27,7 +27,14 @@ const router = new VueRouter({
      name: "movie",
      params: true,
      component: Detailpage}
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
 
 new Vue({
