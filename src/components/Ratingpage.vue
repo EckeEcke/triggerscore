d719328dfe6827ab6be6ecfb4abb6573252daa58
Christian Eckardt <eckardt.christian@web.de>
2021-12-18 14:39:32 +0100

    fixed flex-end overflow issues

--- src/components/Ratingpage.vue
+++ src/components/Ratingpage.vue
@@ -4,7 +4,7 @@
         <h2 class="text-xl text-white bg-red-600 py-4" id="rating">Bewertung abgeben für<br><i class="font-semibold">"{{ title }}"</i></h2>
                     <label class="my-3 block text-lg font-semibold">Sexismus</label>
                     <p class="mb-4">Werden veraltete Rollenbilder vermittelt? </p>
-                    <div class="rating hide-scrollbar flex w-fit justify-end sm:justify-center overflow-y-scroll sm:overflow-hidden mb-5">
+                    <div class="rating hide-scrollbar flex w-fit sm:justify-center overflow-y-scroll sm:overflow-hidden mb-5">
                         <button v-for="(n,i) in 11" :key="i" @click="ratingSexism = ratingSexism == 10-i ? null : 10-i " :class="[['rating' + (10-i)], [ratingSexism == 10-i ? 'opacity-100' : 'opacity-40']]" class="w-16 h-16 sm:w-8 sm:h-8 md:w-12 md:h-12 p-1 text-center text-sm flex-none opacity-50 font-semibold" >
                             <div class="w-full h-full rounded-full flex text-center justify-center" :class="{'transform scale-110': ratingSexism == 10-i, 'bg-green-400': 10-i < 3, 'bg-yellow-400': 10-i > 2 && 10-i < 8, 'bg-red-500': 10-i > 7, 'opacity-100': ratingSexism == 10-i }"><span class="self-center">{{10-i}}</span></div>
                         </button>
@@ -13,7 +13,7 @@
                     <hr>
                     <label class="my-3 block text-lg font-semibold">Rassismus</label>
                     <p class="mb-4">Werden unkritisch rassistische Klischees bedient?</p>
-                    <div class="rating hide-scrollbar flex w-fit justify-end sm:justify-center overflow-y-scroll sm:overflow-hidden mb-5">
+                    <div class="rating hide-scrollbar flex w-fit sm:justify-center overflow-y-scroll sm:overflow-hidden mb-5">
                         <button v-for="(n,i) in 11" :key="i" @click="ratingRacism = ratingRacism == 10-i ? null : 10-i " :class="[['rating' + (10-i)], [ratingRacism == 10-i ? 'opacity-100' : 'opacity-50']]" class="w-16 h-16 sm:w-8 sm:h-8 md:w-12 md:h-12 p-1 text-center text-sm flex-none font-semibold">
                             <div class="w-full h-full rounded-full flex text-center justify-center" :class="{'transform scale-110': ratingRacism == 10-i, 'bg-green-400': 10-i < 3, 'bg-yellow-400': 10-i > 2 && 10-i < 8, 'bg-red-500': 10-i > 7, 'opacity-100': ratingRacism == 10-i }"><span class="self-center">{{10-i}}</span></div>
                         </button>
@@ -21,7 +21,7 @@
                     <hr>
                     <label class="my-3 block text-lg font-semibold">Sonstige</label>
                     <p class="mb-4">Enthält der Film weitere triggernde Elemente?</p>
-                    <div class="rating hide-scrollbar flex w-fit justify-end sm:justify-center overflow-y-scroll sm:overflow-hidden mb-5">
+                    <div class="rating hide-scrollbar flex w-fit sm:justify-center overflow-y-scroll sm:overflow-hidden mb-5">
                         <button v-for="(n,i) in 11" :key="i" @click="ratingOthers = ratingOthers == 10-i ? null : 10-i " :class="[['rating' + (10-i)], [ratingOthers == 10-i ? 'opacity-100' : 'opacity-50']]" class="w-16 h-16 sm:w-8 sm:h-8 md:w-12 md:h-12 p-1 text-center text-sm flex-none font-semibold">
                             <div class="w-full h-full rounded-full flex text-center justify-center" :class="{'transform scale-110': ratingOthers == 10-i, 'bg-green-400': 10-i < 3, 'bg-yellow-400': 10-i > 2 && 10-i < 8, 'bg-red-500': 10-i > 7, 'opacity-100': ratingOthers == 10-i }"><span class="self-center">{{10-i}}</span></div>
                         </button>
@@ -30,7 +30,7 @@
                     <hr>
                     <label class="my-3 block text-lg font-semibold">Cringe</label>
                     <p class="mb-4">Sind Inszenierung, Dialoge oder Handlungen "zum Fremdschämen"?</p>
-                    <div class="rating hide-scrollbar flex w-fit justify-end sm:justify-center overflow-y-scroll sm:overflow-hidden mb-5">
+                    <div class="rating hide-scrollbar flex w-fit sm:justify-center overflow-y-scroll sm:overflow-hidden mb-5">
                         <button v-for="(n,i) in 11" :key="i" @click="ratingCringe = ratingCringe == 10-i ? null : 10-i " :class="[['rating' + (10-i)], [ratingCringe == 10-i ? 'opacity-100' : 'opacity-50']]" class="w-16 h-16 sm:w-8 sm:h-8 md:w-12 md:h-12 p-1 text-center text-sm flex-none font-semibold">
                             <div class="w-full h-full rounded-full flex text-center justify-center" :class="{'transform scale-110': ratingCringe == 10-i, 'bg-green-400': 10-i < 3, 'bg-yellow-400': 10-i > 2 && 10-i < 8, 'bg-red-500': 10-i > 7, 'opacity-100': ratingCringe == 10-i }"><span class="self-center">{{10-i}}</span></div>
                         </button>
@@ -59,6 +59,17 @@ export default {
   props: {
     title: String,
     id: Number
+  },
+  mounted: function() {
+      this.scrollRatings()
+  },
+  methods: {
+      scrollRatings: function() {
+          const ratings = document.getElementsByClassName("rating") // for fixing issues with flex-end and overflow-hidden
+          for (let i = 0; i < ratings.length; i++) {
+              ratings[i].scrollBy(-500, 0)
+          }
+      }
   },
   computed: {
       poster: function() {
