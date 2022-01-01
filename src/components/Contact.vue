c36ebb20d47b8cfa564d345f04b573bc78d521e7
Christian Eckardt <eckardt.christian@web.de>
2022-01-01 22:49:59 +0100

    netlify form settings

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -5,14 +5,14 @@
             <p class="font-semibold">Christian Eckardt</p>
             <p class="mb-8 font-semibold">22301 Hamburg</p>
             <p class="mb-8 font-semibold"><a href="eckeecke.github.io">eckeecke.github.io</a></p>
-            <p class="mb-6 flex">
+            <p class="mb-8 flex">
                 <a href="https://github.com/EckeEcke" target="_blank"><img class="h-10 mr-4" src="../assets/images/github.svg" /></a>
                 <a href="https://www.linkedin.com/in/christian-eckardt-41527216a/" target="_blank"><img class="h-10 mr-4" src="../assets/images/linkedin.svg" /></a>
                 <a href="https://www.xing.com/profile/Christian_Eckardt21/cv" target="_blank"><img class="h-10 mr-4" src="../assets/images/xing.svg" /></a>
             </p>
         </div>
-        <form name="contact" method="POST" data-netlify="true" class="w-full md:w-1/2">
-        <input type="hidden" name="form-name" value="contact" />
+        <form name="contact" action="/contact" method="POST" data-netlify="true" class="w-full md:w-1/2">
+            <input type="hidden" name="form-name" value="contact" />
             <h1 class="mb-4 text-2xl font-semibold">Feedback senden</h1>
             <p class="mb-4">Schick mir eine Nachricht</p>
             <p class="mb-2">
