c5e0602266fd32ea3bd5dd582b3dc90bd1ac3e3d
Christian Eckardt <eckardt.christian@web.de>
2022-01-02 00:10:43 +0100

    netlify form

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -11,24 +11,24 @@
                 <a href="https://www.xing.com/profile/Christian_Eckardt21/cv" target="_blank"><img class="h-10 mr-4" src="../assets/images/xing.svg" /></a>
             </p>
         </div>
-        <form name="contact" action="POST" data-netlify="true" class="w-full md:w-1/2">
+        <form name="contact" action="POST" data-netlify="true" class="w-full md:w-1/2" @submit.prevent="handleSubmit()">
             <input type="hidden" name="form-name" value="contact" />
             <h1 class="mb-4 text-2xl font-semibold">Feedback senden</h1>
             <p class="mb-4">Schick mir eine Nachricht</p>
             <p class="mb-2">
                 <label>Name:</label>
                 <br>
-                <input type="text" name="name" class="border border-gray-300 p-2 rounded w-52" required />   
+                <input v-model="form.name" type="text" name="name" class="border border-gray-300 p-2 rounded w-52" required />   
             </p>
             <p class="mb-2">
                 <label>Email:</label>
                 <br>
-                 <input type="email" name="email" class="border border-gray-300 p-2 rounded w-52" required />
+                 <input v-model="form.mail" type="email" name="email" class="border border-gray-300 p-2 rounded w-52" required />
             </p>
             <p class="mb-2">
                 <label>Nachricht:</label>
                 <br>
-                <textarea name="message" class="border border-gray-300 p-2 rounded w-52" required></textarea>
+                <textarea v-model="form.message" name="message" class="border border-gray-300 p-2 rounded w-52" required></textarea>
             </p>
             <p class="">
                 <button class="bg-yellow-500 p-3 text-gray-900 text-semibold rounded-lg shadow-lg" type="submit">Absenden</button>
@@ -38,7 +38,43 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-  name: 'Contact'
+  name: 'Contact',
+  data(){
+      return {
+          form: {
+              name: '',
+              mail: '',
+              message: ''
+          }
+      }
+  },
+  methods: {
+    encode (data) {
+      return Object.keys(data)
+        .map(
+          key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+        )
+        .join("&");
+    },
+    handleSubmit () {
+      const axiosConfig = {
+        header: { "Content-Type": "application/x-www-form-urlencoded" }
+      };
+      axios.post(
+        "/",
+        this.encode({
+          "form-name": "contact-form",
+          ...this.formData
+        }),
+        axiosConfig
+      ).then(() => {
+        this.$router.push('success')
+      }).catch(() => {
+        this.$router.push('NotFound')
+      });
+    }
+  }
 }
 </script>
