34be60e3c9661ab1fb1b54a8e4a40e378574fc42
Christian Eckardt <eckardt.christian@web.de>
2022-03-09 22:30:51 +0100

    hiding swiper btns on mobile

--- src/components/MovieHighlightsContainer.vue
+++ src/components/MovieHighlightsContainer.vue
@@ -7,7 +7,7 @@
         <div class="relative group">
             <div id="highlight-container" class="hide-scrollbar flex flex-none flex-row  overflow-x-scroll py-4 px-0 md:px-4 -mx-4">
                 <MovieHighlightItem v-for="movie in bondMovies" :key="movie.id" :movie="movie" :scores="triggerscores[triggerscores.map(score => score.movie_id).indexOf(movie.id)]" />
-                <div class="justify-between md:px-4 container mx-auto xl:w-10/12 hidden  group-hover:flex pointer-events-none absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <div class="justify-between md:px-4 container mx-auto xl:w-10/12 hidden  sm:group-hover:flex pointer-events-none absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                     <button @click="scrollHighlightContainer('left')" class="bg-white rounded-full"><font-awesome-icon class="text-yellow-500 text-4xl transition transform scale-110 hover:scale-125 pointer-events-auto" :icon="['fas', 'arrow-circle-left']" /></button>
                     <button @click="scrollHighlightContainer('right')" class="bg-white rounded-full"><font-awesome-icon class="text-yellow-500 text-4xl transition transform scale-110 hover:scale-125 pointer-events-auto" :icon="['fas', 'arrow-circle-right']" /></button>
                 </div> 
