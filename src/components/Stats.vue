1d1dfc432ce24d703a1b19c7e932fc466e96e634
Christian Eckardt <eckardt.christian@web.de>
2022-03-15 23:47:45 +0100

    starting new stats component

---
+++ src/components/Stats.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container-xl bg-white p-6 md:p-12 text-left md:my-8 text-sm md:w-8/12 lg:w-1/2 mx-auto md:rounded-lg">
+        <div class="bg-white rounded-lg">
+                        
+        </div>
+        <div class="bg-white rounded-lg">
+              <MovieListitem />
+              <MovieListitem />          
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Stats'
+}
+</script>
