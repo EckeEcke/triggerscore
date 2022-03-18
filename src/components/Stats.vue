60336373119edf56c98256e4cefaa72a1c40d9a8
Christian Eckardt <eckardt.christian@web.de>
2022-03-18 21:27:02 +0100

    adding gap for stats

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,13 +1,13 @@
 <template>
     <section class="container-xl bg-white p-6 md:p-12 text-left md:my-8 text-sm md:w-8/12 lg:w-1/2 mx-auto md:rounded-lg">
         <div class="bg-white rounded-lg font-semibold">
-          <div class="flex justify-between items-center border-b border-gray-300 py-4 h-24">
+          <div class="flex justify-between items-center border-b border-gray-300 py-4 h-24 gap-4">
             <span class="text-lg">Gesamtzahl abgegebene Bewertungen:</span><span class="text-3xl font-semibold">{{ stats.totalRatings }}</span>
           </div>
-          <div class="flex justify-between items-center border-b border-gray-300 py-4 h-24">
+          <div class="flex justify-between items-center border-b border-gray-300 py-4 h-24 gap-4">
             <span class="text-lg">Filme auf Triggerscore:</span><span class="text-3xl font-semibold">{{ stats.amountMovies }}</span>
           </div>
-          <div class="flex justify-between items-center border-b border-gray-300 py-4">
+          <div class="flex justify-between items-center border-b border-gray-300 py-4 gap-4">
             <span class="text-lg">Durchschnitt Triggerscore:</span>
             <div class="h-16 w-16 text-white rounded-lg font-semibold text-3xl" :class="{'bg-red-700': stats.averageScoreTotal >= 7, 'bg-yellow-400':  stats.averageScoreTotal < 7 &&  stats.averageScoreTotal >=4, 'bg-green-500':  stats.averageScoreTotal < 4}">
                 <div class="relative w-full h-full">
@@ -15,7 +15,7 @@
                 </div>
             </div>
           </div>
-          <div class="flex justify-between items-center border-b border-gray-300 py-4">
+          <div class="flex justify-between items-center border-b border-gray-300 py-4 gap-4">
             <span class="text-lg">Durchschnitt Sexismus:</span>
             <div class="h-16 w-16 text-white rounded-lg font-semibold text-3xl" :class="{'bg-red-700': stats.averageScoreSexism >= 7, 'bg-yellow-400':  stats.averageScoreSexism < 7 &&  stats.averageScoreSexism >=4, 'bg-green-500':  stats.averageScoreSexism < 4}">
                 <div class="relative w-full h-full">
@@ -23,7 +23,7 @@
                 </div>
             </div>
           </div>
-          <div class="flex justify-between items-center border-b border-gray-300 py-4">
+          <div class="flex justify-between items-center border-b border-gray-300 py-4 gap-4">
             <span class="text-lg">Durchschnitt Rassismus:</span>
             <div class="h-16 w-16 text-white rounded-lg font-semibold text-3xl" :class="{'bg-red-700': stats.averageScoreRacism >= 7, 'bg-yellow-400':  stats.averageScoreRacism < 7 &&  stats.averageScoreRacism >=4, 'bg-green-500':  stats.averageScoreRacism < 4}">
                 <div class="relative w-full h-full">
@@ -31,7 +31,7 @@
                 </div>
             </div>
           </div>
-          <div class="flex justify-between items-center border-b border-gray-300 py-4">
+          <div class="flex justify-between items-center border-b border-gray-300 py-4 gap-4">
             <span class="text-lg">Durchschnitt Sonstige:</span>
             <div class="h-16 w-16 text-white rounded-lg font-semibold text-3xl" :class="{'bg-red-700': stats.averageScoreOthers >= 7, 'bg-yellow-400':  stats.averageScoreOthers < 7 &&  stats.averageScoreOthers >=4, 'bg-green-500':  stats.averageScoreOthers < 4}">
                 <div class="relative w-full h-full">
@@ -39,7 +39,7 @@
                 </div>
             </div>
           </div>
-          <div class="flex justify-between items-center border-b border-gray-300 py-4">
+          <div class="flex justify-between items-center border-b border-gray-300 py-4 gap-4">
             <span class="text-lg">Durchschnitt Cringe:</span>
             <div class="h-16 w-16 text-white rounded-lg font-semibold text-3xl" :class="{'bg-red-700': stats.averageScoreCringe >= 7, 'bg-yellow-400':  stats.averageScoreCringe < 7 &&  stats.averageScoreCringe >=4, 'bg-green-500':  stats.averageScoreCringe < 4}">
                 <div class="relative w-full h-full">
