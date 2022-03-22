891376ff0e202ee52e1630cde08e8dc3dd03894f
Christian Eckardt <eckardt.christian@web.de>
2022-03-22 20:03:33 +0100

    styling of sub pages

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="container bg-white px-4 py-6 md:py-12 text-left xl:w-10/12 mx-auto md:rounded-lg flex justify-start flex-wrap gap-12">
+    <div class="container text-white px-4 py-6 md:py-12 text-left xl:w-10/12 mx-auto md:rounded-lg flex justify-start flex-wrap gap-12">
         <div v-if="!submitted" class="mr-8 max-w-full">
-            <h1 class="mb-4 text-2xl font-semibold">Feedback senden</h1>
+            <h1 class="mb-4 text-xl md:text-2xl font-semibold">Feedback senden</h1>
             <form name="contact" class="w-full p-8 bg-red-900 rounded-lg" method="post"
                 data-netlify="true"
                 data-netlify-honeypot="bot-field" @submit.prevent="handleSubmit"
@@ -28,12 +28,12 @@
             <p class="p-4 bg-green-500 text-white rounded-lg shadow-lg font-semibold text-center">Nachricht erfolgreich versendet <font-awesome-icon :icon="['fas', 'check']" class="text-white" /></p>
         </div>
         <div class="">
-            <h1 class="mb-4 text-2xl font-semibold">Kontakt</h1>
+            <h1 class="mb-4 text-xl md:text-2xl font-semibold">Kontakt</h1>
             <p class="font-semibold">Christian Eckardt</p>
             <p class="mb-8 font-semibold">22301 Hamburg</p>
             <p class="mb-8 font-semibold"><a href="https://eckeecke.github.io">eckeecke.github.io</a></p>
             <p class="mb-8 flex">
-                <a href="https://github.com/EckeEcke" target="_blank"><img class="h-10 mr-4" src="../assets/images/github.svg" /></a>
+                <a href="https://github.com/EckeEcke" target="_blank"><img class="h-10 mr-4 filter invert" src="../assets/images/github.svg" /></a>
                 <a href="https://www.linkedin.com/in/christian-eckardt-41527216a/" target="_blank"><img class="h-10 mr-4" src="../assets/images/linkedin.svg" /></a>
                 <a href="https://www.xing.com/profile/Christian_Eckardt21/cv" target="_blank"><img class="h-10 mr-4" src="../assets/images/xing.svg" /></a>
             </p>
