9be476064fcf1c77abc15613da084e4ee1a51a07
Christian Eckardt <eckardt.christian@web.de>
2022-03-31 22:25:47 +0200

    new styling for recent ratings

--- src/components/RecentRatingsItem.vue
+++ src/components/RecentRatingsItem.vue
@@ -1,16 +1,25 @@
 <template>
-    <router-link :to="{name: 'movie', params: {id: movie.id}}" tag="div" class="bg-white md:rounded-lg shadow-md flex relative border-b-2 md:border-b-0 md:rounded-lg cursor-pointer min-w-sm md:hover:scale-105 transform transition duration-300 sm:first:rounded-t-lg sm:last:rounded-b-lg">
-        <div class="w-full max-w-sm p-3">
-            <article class="text-left relative w-full h-full">
-                <div class="flex items-center gap-2">
+    <router-link :to="{name: 'movie', params: {id: movie.id}}" tag="div" class="bg-white md:rounded-lg shadow-md flex flex-col relative border-b-2 md:border-b-0 md:rounded-lg cursor-pointer min-w-sm md:hover:scale-105 transform transition duration-300 sm:first:rounded-t-lg sm:last:rounded-b-lg">
+        <div class="hidden md:block relative w-full h-44 p-3 text-white bg-cover bg-center md:rounded-t-lg w-3/4" :style="{ backgroundImage: 'linear-gradient(to bottom, rgba(22,0,0,0.1),rgba(22,0,0,0.2)),url(' + poster + ')'}">
                  
-                <div class="flex rounded-lg justify-center w-10 h-10 lg:w-12 lg:h-12 mr-2" :class="{'bg-red-700': scoreTotal >= 7, 'bg-yellow-400': scoreTotal < 7 && scoreTotal >=4, 'bg-green-500': scoreTotal < 4}">
+                <div class="flex absolute top-2 right-2 rounded-lg justify-center w-16 h-16 mr-2 bg-opacity-80" :class="{'bg-red-700': scoreTotal >= 7, 'bg-yellow-400': scoreTotal < 7 && scoreTotal >=4, 'bg-green-500': scoreTotal < 4}">
+                    <p class="self-center text-white text-xl font-semibold">{{scoreTotal}}</p>
+                </div>
+        </div>
+        <div class="md:hidden p-3 pb-0 bg-white text-inherit">
+            <div class="flex items-center gap-2 text-left">
+                 
+                <div class="flex rounded-lg justify-center w-10 h-10 mr-2" :class="{'bg-red-700': scoreTotal >= 7, 'bg-yellow-400': scoreTotal < 7 && scoreTotal >=4, 'bg-green-500': scoreTotal < 4}">
                     <p class="self-center text-white text-lg font-semibold">{{scoreTotal}}</p>
                 </div>
                    <h3 v-if="movie.title.length > 0" class="flex items-center text-base mb-1 font-semibold overflow-hidden inline-block h-12 overflow-hidden w-10/12">{{ movie.title }}</h3>
                 <h3 v-else class="text-base mb-1 font-semibold overflow-hidden w-10/12">{{ movie.original_title }}</h3>
                 </div>
-                <hr class="hidden sm:block my-1 sm:my-3">
+        </div>
+        <div class="w-full max-w-sm p-3">
+            <article class="text-left relative w-full h-full">
+                <h3 v-if="movie.title.length > 0" class="hidden md:block text-base mb-1 font-semibold overflow-hidden h-12 overflow-hidden">{{ movie.title }}</h3>
+                <h3 v-else class="hidden md:block text-base mb-1 font-semibold overflow-hidden">{{ movie.original_title }}</h3>
                 <div class="grid grid-cols-2 text-sm">
                     <div class="flex my-2 text-sm">
                             <div class="flex justify-center rounded-lg w-8 h-8 mr-2 bg-opacity-80"  :class="{'bg-red-700': scores.rating_sexism >= 7, 'bg-yellow-400': scores.rating_sexism < 7 && scores.rating_sexism >=4, 'bg-green-500': scores.rating_sexism < 4}">
@@ -50,7 +59,9 @@ export default {
     scores: Object
   },
   computed: {
-
+      poster: function() {
+          return `https://image.tmdb.org/t/p/original/${this.movie.backdrop_path}`
+      },
       scoreAvailable: function() {
           return this.scores != undefined
       },
