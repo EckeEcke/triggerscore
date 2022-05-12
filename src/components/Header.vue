fdb574d13ead4123c2a2cf7e2e42c5f764b85398
Christian Eckardt <eckardt.christian@web.de>
2022-05-12 21:02:59 +0200

    minor fixes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,15 +13,15 @@
               <router-link to="/contact" tag="a" class="text-white font-semibold self-center mr-6 sm:mr-10 hidden md:block hover:text-yellow-500">{{ $t('header.contact') }}</router-link>
               <button v-if="$root.$i18n.locale == 'en'" @click="switchLanguage('de')" class="mr-6 sm:mr-10"><img class="w-6" src="../assets/images/germany.svg"></button>
               <button v-if="$root.$i18n.locale == 'de'" @click="switchLanguage('en')" class="mr-6 sm:mr-10"><img class="w-6" src="../assets/images/uk.svg"></button>
-              <font-awesome-icon :icon="['fas', 'search']" class="text-lg text-white mr-6 sm:mr-10 self-center hover:text-yellow-500" :class="{'animate-bounce': showSearch}" @click="showSearch = !showSearch; showMenu = false;showNav = false" />
+              <font-awesome-icon :icon="['fas', 'search']" class="text-lg text-white mr-6 sm:mr-0 self-center hover:text-yellow-500" :class="{'animate-bounce': showSearch}" @click="showSearch = !showSearch; showMenu = false;showNav = false" />
               <font-awesome-icon :icon="['fas', 'bars']" class="text-white text-lg self-center md:hidden" @click="showNav = !showNav; showMenu = false; showSearch = false" />
             </div>
             
         </section>
         <transition-group  enter-active-class="duration-300 ease-out"
                 enter-class="opacity-0" enter-to-class="opacity-100" leave-active-class="duration-500 ease-in" leave-class="opacity-100" leave-to-class="opacity-0">
-                 <div v-if="showNav" key="backdrop" class="fixed bg-gray-900 bg-opacity-20 top-0 left-0 w-full h-screen overflow-none"  @click="showNav = false" />
-              <nav v-if="showNav" key="sidebar" class="w-80 bg-white h-screen absolute top-0 right-0 shadow-lg md:hidden z-40">
+                 <div v-if="showNav" key="backdrop" class="fixed bg-gray-900 bg-opacity-40 top-0 left-0 w-full h-screen overflow-none"  @click="showNav = false" />
+              <nav v-if="showNav" key="sidebar" class="w-80 bg-gray-900 h-screen absolute top-0 right-0 shadow-lg md:hidden z-40">
                 <div class="text-right text-xl p-4 bg-gray-900 text-white">
                   <font-awesome-icon :icon="['fas', 'times']" @click="showNav = false" />
                 </div>
@@ -31,29 +31,32 @@
                 </router-link> 
                 </div>
                 <hr>
-                <router-link to="/" tag="div" class="text-lg font-semibold self-center hover:text-yellow-500"><div class="py-6" @click="showNav = false">{{ $t('header.home') }}</div></router-link>  
+                <router-link to="/" tag="div" class="text-lg text-white font-semibold self-center hover:text-yellow-500"><div class="py-6" @click="showNav = false">{{ $t('header.home') }}</div></router-link>  
                 <hr>
-                <router-link to="/all" tag="div" class="text-lg font-semibold self-center hover:text-yellow-500"><div class="py-6" @click="showNav = false">{{ $t('header.allMovies') }}</div></router-link>
+                <router-link to="/all" tag="div" class="text-lg text-white font-semibold self-center hover:text-yellow-500"><div class="py-6" @click="showNav = false">{{ $t('header.allMovies') }}</div></router-link>
                 <hr>
-                <router-link to="/faq" tag="div" class="text-lg font-semibold self-center hover:text-yellow-500"><div class="py-6" @click="showNav = false">{{ $t('header.faq') }}</div></router-link>
+                <router-link to="/faq" tag="div" class="text-lg text-white font-semibold self-center hover:text-yellow-500"><div class="py-6" @click="showNav = false">{{ $t('header.faq') }}</div></router-link>
                 <hr>
-                <router-link to="/about" tag="div" class="text-lg font-semibold self-center hover:text-yellow-500"><div class="py-6" @click="showNav = false">{{ $t('header.about') }}</div></router-link>
+                <router-link to="/about" tag="div" class="text-lg text-white font-semibold self-center hover:text-yellow-500"><div class="py-6" @click="showNav = false">{{ $t('header.about') }}</div></router-link>
                 <hr>
-                <router-link to="/contact" tag="div" class="text-lg font-semibold self-center hover:text-yellow-500"><div class="py-6" @click="showNav = false">{{ $t('header.contact') }}</div></router-link>
+                <router-link to="/contact" tag="div" class="text-lg text-white font-semibold self-center hover:text-yellow-500"><div class="py-6" @click="showNav = false">{{ $t('header.contact') }}</div></router-link>
                 <hr>
               </nav>
             </transition-group>
+            <div v-if="showSearch" class="fixed h-screen w-screen top-0 left-0 bg-gray-900 bg-opacity-40 overflow-none" @click="showSearch = false"></div>
         <transition enter-active-class="duration-300 ease-out"
                 enter-class="opacity-0" enter-to-class="opacity-100" leave-active-class="duration-500 ease-in" leave-class="opacity-100" leave-to-class="opacity-0">
-          <section v-if="showSearch">
-            <div class="container flex flex-col md:flex-row mx-auto md:px-4 xl:w-10/12 absolute top-full left-1/2 transform -translate-x-1/2 bg-red-700">
-              <div class="flex mx-auto p-4 h-16 self-center w-full">
-                <div class="rounded flex w-full justify-start">
-                  <button class="flex items-center justify-center px-4 rounded-l bg-yellow-500" @click="searchMovie">
+                
+          <section v-if="showSearch" class="w-full relative">
+            <div class="absolute w-full h-32 bg-red-700"></div>
+            <div class="container flex flex-col md:flex-row mx-auto md:px-4 xl:w-10/12 absolute top-0 left-1/2 transform -translate-x-1/2 bg-red-700 h-32">
+              <div class="flex mx-auto mt-6 p-4 sm:px-0 h-20 self-center w-full">
+                <div class="rounded-lg flex w-full justify-start">
+                  <button class="flex items-center justify-center px-3 w-16 rounded-l-xl bg-yellow-500" @click="searchMovie">
                       <font-awesome-icon :icon="['fas', 'search']" class="text-lg" />
                   </button>
-                  <input type="text" v-model="searchInput" v-on:keyup.enter="searchMovie" @input="resetSearchResults" class="px-4 w-full outline-none transition" :placeholder="$t('header.searchPlaceholder')">
-                  <div class="bg-white rounded-r h-8 w-8 flex justify-center">
+                  <input type="text" v-model="searchInput" v-on:keyup.enter="searchMovie" @input="resetSearchResults" class="lg:text-xl px-4 w-full outline-none transition" :placeholder="$t('header.searchPlaceholder')">
+                  <div class="bg-white rounded-r-xl h-full w-8 flex justify-center">
                     <font-awesome-icon :icon="['fas', 'times']" class="self-center" @click="showSearch = false" />
                   </div>
                 </div>
