65017ece894f08b72b1fb4271f05ba87f67fa76e
Christian Eckardt <eckardt.christian@web.de>
2022-09-07 11:08:45 +0200

    minor stylings

--- src/components/FAQ.vue
+++ src/components/FAQ.vue
@@ -3,19 +3,19 @@
 <div class="container text-white py-6 px-4 md:py-12 xl:w-10/12 text-left text-sm md:text-base mx-auto md:rounded-lg">
         <h2 class="font-semibold text-xl md:text-2xl mb-2">{{ $t('faq.headline') }}</h2>
         <hr class="my-4">
-        <section class="mt-4 lg:w-3/4 leading-7">
+        <section class="mt-4 lg:w-3/4 leading-6 md:leading-7">
             <p class="mb-2"><b>{{ $t('faq.question1') }}</b></p>
             <p>{{ $t('faq.answer1') }}</p>
         </section>
-        <section class="mt-4 lg:w-3/4 leading-7">
+        <section class="mt-4 lg:w-3/4 leading-6 md:leading-7">
             <p class="mb-2"><b>{{ $t('faq.question2') }}</b></p>
             <p>{{ $t('faq.answer2') }}</p>
         </section>
-        <section class="mt-4 lg:w-3/4 leading-7">
+        <section class="mt-4 lg:w-3/4 leading-6 md:leading-7">
             <p class="mb-2"><b>{{ $t('faq.question3') }}</b></p>
             <p>{{ $t('faq.answer3') }}</p>
         </section>
-        <section class="mt-4 lg:w-3/4 leading-7">
+        <section class="mt-4 lg:w-3/4 leading-6 md:leading-7">
             <p class="mb-2"><b>{{ $t('faq.question4') }}</b></p>
             <p>{{ $t('faq.answer4') }}</p>
         </section>
--- src/components/RecentRatingsItem.vue
+++ src/components/RecentRatingsItem.vue
@@ -3,7 +3,7 @@
         <div class="hidden md:block relative w-full h-44 p-2 text-white sm:rounded-t w-3/4 overflow-hidden">
             <div class="absolute top-0 left-0 w-full h-full bg-cover bg-center movie-poster" :style="{ backgroundImage: 'linear-gradient(to bottom, rgba(22,0,0,0.1),rgba(22,0,0,0.2)),url(' + poster + ')'}" />
             <div v-if="totalRatings[0].ratings == 1" class="flex items-center w-16 h-16 bg-gradient-to-br from-black to-gray-900 bg-opacity-80 text-yellow-500 rounded-full justify-center drop-shadow">
-                <span class="font-semibold  transform -rotate-12">{{ $t('general.new') }}</span>
+                <span class="font-semibold  transform -rotate-12 italic">{{ $t('general.new') }}</span>
             </div>  
             <div class="flex absolute top-2 right-2 rounded-lg justify-center w-16 h-16 mr-2 bg-opacity-80" :class="{'bg-red-700': scoreTotal >= 7, 'bg-yellow-500': scoreTotal < 7 && scoreTotal >=4, 'bg-green-600': scoreTotal < 4}">
                 <p class="self-center text-white text-xl font-semibold">{{scoreTotal}}</p>
