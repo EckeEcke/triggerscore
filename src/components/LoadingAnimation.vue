189a983fa5cc956e82c3636267780f41298a4335
Christian Eckardt <eckardt.christian@web.de>
2022-09-13 22:59:56 +0200

    replacing loading animation

--- src/components/LoadingAnimation.vue
+++ src/components/LoadingAnimation.vue
@@ -2,8 +2,9 @@
     <div class="max-w-screen p-16 relative flex justify-center mx-auto animate-pulse" style="width:600px;max-width:100vw">
         <lottie-animation
             ref="anim"
-            :animationData="require('@/assets/images/loading-animation.json')"
+            :animationData="require('@/assets/images/loading-animation3.json')"
             :loop=true
+            style="fill:white"
         />
         <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2">
             <p class="text-white font-semibold text-4xl">{{  $t('general.loadMovies')}}</p>
