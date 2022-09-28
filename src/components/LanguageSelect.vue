ba79d60c770673b4fafa4934b9ca0ea7add44fee
Christian Eckardt <eckardt.christian@web.de>
2022-09-28 14:26:54 +0200

    adding french text ressources

--- src/components/LanguageSelect.vue
+++ src/components/LanguageSelect.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="relative flex align-center">
-        <button v-if="$root.$i18n.locale == 'de'"  @click="showSelection = !showSelection" class="mr-6 sm:mr-10"><img class="w-6" src="../assets/images/germany.svg"></button>
-        <button v-if="$root.$i18n.locale == 'en'"  @click="showSelection = !showSelection" class="mr-6 sm:mr-10"><img class="w-6" src="../assets/images/uk.svg"></button>
-        <button v-if="$root.$i18n.locale == 'us'"  @click="showSelection = !showSelection" class="mr-6 sm:mr-10"><img class="w-6" src="../assets/images/usa.svg"></button>
+        <button v-if="$root.$i18n.locale == 'de'"  @click="showSelection = !showSelection" class="mr-6 sm:mr-10"><img class="w-6 h-4" src="../assets/images/germany.svg"></button>
+        <button v-if="$root.$i18n.locale == 'en'"  @click="showSelection = !showSelection" class="mr-6 sm:mr-10"><img class="w-6 h-4" src="../assets/images/uk.svg"></button>
+        <button v-if="$root.$i18n.locale == 'us'"  @click="showSelection = !showSelection" class="mr-6 sm:mr-10"><img class="w-6 h-4" src="../assets/images/usa.svg"></button>
+        <button v-if="$root.$i18n.locale == 'fr'"  @click="showSelection = !showSelection" class="mr-6 sm:mr-10"><img class="w-6 h-4" src="../assets/images/france.svg"></button>
         <ul v-if="showSelection" v-on-clickaway="hideSelection" class="absolute -left-2 bg-gradient-to-r from-gray-950 to-gray-800 text-white top-9 md:top-10 p-2 flex flex-col gap-2 w-20">
-            <li @click="switchLanguage('us')" class="flex gap-2 cursor-pointer hover:text-yellow-500"><img class="w-6" src="../assets/images/usa.svg">US</li>
-            <li @click="switchLanguage('en')" class="flex gap-2 cursor-pointer hover:text-yellow-500"><img class="w-6" src="../assets/images/uk.svg">EN</li>
-            <li @click="switchLanguage('de')" class="flex gap-2 cursor-pointer hover:text-yellow-500"><img class="w-6" src="../assets/images/germany.svg">DE</li>
+            <li @click="switchLanguage('us')" class="flex gap-2 cursor-pointer hover:text-yellow-500"><img class="w-6 self-center h-3" src="../assets/images/usa.svg">US</li>
+            <li @click="switchLanguage('en')" class="flex gap-2 cursor-pointer hover:text-yellow-500"><img class="w-6 self-center h-3" src="../assets/images/uk.svg">EN</li>
+            <li @click="switchLanguage('de')" class="flex gap-2 cursor-pointer hover:text-yellow-500"><img class="w-6 self-center h-3" src="../assets/images/germany.svg">DE</li>
+            <li @click="switchLanguage('fr')" class="flex gap-2 cursor-pointer hover:text-yellow-500"><img class="w-6 self-center h-3" src="../assets/images/france.svg">FR</li>
         </ul>
     </div>
 </template>
