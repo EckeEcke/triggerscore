d0637474d8cd7234e5c4c37ad427da158f546708
Christian Eckardt <eckardt.christian@web.de>
2023-02-14 23:52:46 +0100

    adding placeholder json and stylings recentratings

--- src/components/RecentRatingsItem.vue
+++ src/components/RecentRatingsItem.vue
@@ -9,13 +9,13 @@
                 <p class="self-center text-white text-xl font-semibold">{{scoreTotal}}</p>
             </div>
         </div>
-        <div class="md:hidden sm:rounded-t p-3 pt-6 sm:pt-3 pb-0 bg-transparent text-inherit">
+        <div class="md:hidden sm:rounded-t p-3 pb-0 bg-transparent text-inherit">
             <div class="flex items-center gap-2 text-left">
                  
                 <div class="flex rounded-lg justify-center w-10 h-10 mr-2" :class="{'bg-red-700': scoreTotal >= 7, 'bg-yellow-500': scoreTotal < 7 && scoreTotal >=4, 'bg-green-600': scoreTotal < 4}">
                     <p class="self-center text-white text-lg font-semibold">{{scoreTotal}}</p>
                 </div>
-                <div class=" w-10/12">
+                <div class=" w-9/12">
                     <h3 v-if="movie.title.length > 0" class="text-base font-semibold overflow-hidden h-6 overflow-hidden whitespace-nowrap custom-headline">{{ movie.title }}</h3>
                     <h3 v-else class="text-base font-semibold overflow-hidden">{{ movie.original_title }}</h3>
                     <div class="text-xs pt-1">
@@ -27,9 +27,9 @@
                    
                 </div>
         </div>
-        <div class="w-full px-3 md:px-0 py-3 pt-3 pb-6 sm:pb-2 flex gap-4">
+        <div class="w-full px-3 md:px-0 pt-4 pb-3 flex gap-4">
             <img class="h-36 hidden md:block rounded-sm" :alt="movie.original_title" :src="poster2">
-            <article class="text-left relative w-full h-full">
+            <article class="text-left relative w-full h-full flex flex-col">
                 <h3 v-if="movie.title.length > 0" class="hidden md:block text-base mb-1 font-semibold overflow-hidden h-6 overflow-hidden">{{ movie.title }}</h3>
                 <h3 v-else class="hidden md:block text-base mb-1 font-semibold overflow-hidden">{{ movie.original_title }}</h3>
                 <div class="text-xs mb-1 py-1 hidden md:block">
@@ -37,26 +37,26 @@
                     <span class="mx-2">|</span>
                     <span>{{ movie.runtime }} {{ $t('general.minutes') }}</span>
                 </div> 
-                <div class="grid grid-cols-2 text-sm max-w-xs">
-                    <div class="flex my-2 text-sm">
+                <div class="grid grid-cols-2 text-sm max-w-xs mt-auto">
+                    <div class="flex my-1 text-sm">
                             <div class="flex justify-center rounded-lg w-8 h-8 mr-2 bg-opacity-80"  :class="{'bg-red-700': scores.rating_sexism >= 7, 'bg-yellow-500': scores.rating_sexism < 7 && scores.rating_sexism >=4, 'bg-green-600': scores.rating_sexism < 4}">
                                 <div class="self-center text-white">{{scores.rating_sexism}}</div>
                             </div>
                             <p class="self-center">{{ $t('categories.sexism') }}</p>
                         </div>
-                        <div class="flex my-2 text-sm">
+                        <div class="flex my-1 text-sm">
                             <div class="flex bg-red-500 rounded-lg justify-center w-8 h-8 mr-2 bg-opacity-80" :class="{'bg-red-700': scores.rating_racism >= 7, 'bg-yellow-500': scores.rating_racism < 7 && scores.rating_racism >=4, 'bg-green-600': scores.rating_racism < 4}">
                                 <div class="self-center text-white">{{scores.rating_racism}}</div>
                             </div>
                             <p class="self-center">{{ $t('categories.racism') }}</p>
                         </div>
-                        <div class="flex my-2 text-sm">
+                        <div class="flex my-1 text-sm">
                             <div class="flex bg-red-500 rounded-lg justify-center w-8 h-8 mr-2 bg-opacity-80" :class="{'bg-red-700': scores.rating_others >= 7, 'bg-yellow-500': scores.rating_others < 7 && scores.rating_others >=4, 'bg-green-600': scores.rating_others < 4}">
                                 <div class="self-center text-white">{{scores.rating_others}}</div>
                             </div>
                             <p class="self-center">{{ $t('categories.others') }}</p>
                         </div>
-                        <div class="flex my-2 text-sm">
+                        <div class="flex my-1 text-sm">
                             <div class="flex bg-red-500 rounded-lg justify-center w-8 h-8 mr-2 bg-opacity-80" :class="{'bg-red-700': scores.rating_cringe >= 7, 'bg-yellow-500': scores.rating_cringe < 7 && scores.rating_cringe >=4, 'bg-green-600': scores.rating_cringe < 4}">
                                 <div class="self-center text-white">{{scores.rating_cringe}}</div>
                             </div> 
